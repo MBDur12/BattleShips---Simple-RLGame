d6196b727b3e5cd4f86bf8202b638ed4b906ba86
Michael Bailey <rgfb35@durham.ac.uk>
2022-03-08 09:24:52 +0000

    Add checks of continuity to make valid placement

--- Battleships/Board.py
+++ Battleships/Board.py
@@ -1,12 +1,14 @@
 import numpy as np
 from random import randint
+from itertools import product, groupby
+from operator import itemgetter
 
 class Board():
     def __init__(self, width):
         self.width = width
         self.board = np.zeros((width, width), dtype=int)
         # Store all positions occupied by ships
-        self.ships = {}
+        self._ships = {}
     def display(self):
         for row in self.board:
             print("-" * (4 * self.width + 1))
@@ -14,15 +16,17 @@ class Board():
             for col in row:
                 if col == 0:
                     print(" ", end="")
-                else:
+                elif col == -1:
                     print("X", end="")
+                else:
+                    print("O", end="")
                 print(" | ", end="")
             print("")
         print("-" * (4 * self.width + 1))
 
     # Check if a given position is not already occuped and is a valid placement (i.e., within bounds)
     def _valid_position(self, pos):
-        for coords in self.ships.values():
+        for coords in self._ships.values():
             if pos in coords:
                 return False
         
@@ -31,62 +35,100 @@ class Board():
                 return False
 
         return True
+
+    
         
-    def place_ship(self, length, name):
-        if length <= 0:
-            print("Invalid Length")
-            return
+    def generate_position(self, coords, ship_length, orientation, anchor): 
+        if len(coords) < ship_length:
+            return False
 
-        x = randint(0, self.width)
-        y = randint(0, self.width)
+        pos = []
 
-        while not self._valid_position([x,y]):
-            x = randint(0, self.width)
-            y = randint(0, self.width)
+        if orientation == 0:
+            line = [coord[1] for coord in coords if coord[0] == anchor]
+        else:
+            line = [coord[0] for coord in coords if coord[1] == anchor]
+        
+        cont_coords = []
+        for key, group in groupby(enumerate(line), lambda x: x[0] - x[1]):
+            cont_coords.append(list(map(itemgetter(1), group)))
 
-        self.ships[name] = [[x,y]]
-        x_or_y = randint(0,1)
-        for i in range(1, length):
-            if x_or_y == 0:
-                if self._valid_position([x-1,y]):
-                    self.ships[name].append([x-1,y])
-                else:
-                    self.ships[name].append([x+1,y])
-            if x_or_y == 1:
-                if self._valid_position([x,y-1]):
-                    self.ships[name].append([x,y-1])
-                else:
-                    self.ships[name].append([x,y+1])
+        for set in cont_coords:
+            if len(set) == ship_length:
+                pos = set
+                break
+            elif len(set) > ship_length:
+                pos = set[:ship_length]
+                break
+
+        if not pos:
+
+            return False
+        
+        if orientation == 0:
+            pos = [coord for coord in coords if coord[0] == anchor and coord[1] in pos]
+        else:
+            pos = [coord for coord in coords if coord[1] == anchor and coord[0] in pos]
+
+   
+        return pos
+        
+        
+            
+    def place_ship(self, ship_name, ship_length):
+        
+        # Generate all coordinates and then filter the positions (coords) that are currently unoccupied
+        coords = list(product(range(self.width), repeat=2))
+        spaces = [coord for coord in coords if self._valid_position(coord)]
+
+        while True:
+            anchor_val = randint(0,self.width-1)
+            orientation = randint(0,1)
+            
+            set_pos = self.generate_position(spaces, ship_length, orientation, anchor_val)
+            if set_pos:
+                break
+        
+        self._ships[ship_name] = set_pos
+
+        
+        """
+        How can I handle a failure? Really, how can I make it run until it can validly place the ship?
+        """
+
     
     # Changes values in self.board depending on if a position is hit or not - reveals information.
+    def _check_sunk(self, ship):
+        if not ship:
+            self._ships.pop(ship, None)
+        
+        return True
+
+    def _check_hit(self, ship, coords, pos):
+        if pos in coords:
+            coords.remove(pos)
+            self.board[pos[0], pos[1]] = 1
+            return True
+        
+
     def hit(self, pos):
-        for k,v in self.ships.items():
-            if pos in v:
-                v.remove(pos)
-                self.board[pos[0], pos[1]] = 1
-                print("Hit!") 
-                if not v:
-                    self.ships.pop(k, None)
+        for k,v in self._ships.items():
+            if self._check_hit(k, v, pos):
+                print("Hit!")
+            
+                if self._check_sunk(k):
                     print("Ship Sunk!")
-                    return "Sunk"
+            else:
+                self.board[pos[0], pos[1]] = -1
+                print("Miss!")
 
-                return "Hit"
-        
-        print("Miss!")
-        self.board[pos[0], pos[1]] = -1
-        return "Miss"
-
-        
-
-
-        
 
 
 
 b = Board(5)
 
-b.ships["a"] = [[0,1], [1,1]]
-print(b.ships)
-b.hit([0,1])
-b.hit([1,1])
-print(b.board)
+b.place_ship("carrier", 2)
+b.place_ship("destroyer", 3)
+print(b._ships)
+
+
