a2c171e4b9b97c1f388117729d2dfeb41770b52e
Michael Bailey <rgfb35@durham.ac.uk>
2022-04-01 11:38:10 +0100

    Setup AI class to run MCTS before making moves

---
+++ MCT/AI.py
@@ -0,0 +1,20 @@
+import copy
+
+
+class AI():
+    def __init__(self, env, limit):
+        self.env = env
+        self.limit = limit # This could be a time limit (module) or a limit on # of moves
+
+    # This is where the MCTS is run by the AI, before it selects a move
+    def monte_carlo_search(self):
+        pass
+
+
+
+
+
+
+    def move(self):
+        # Make a move based on running the MCTS
+        pass
--- MCT/main.py
+++ MCT/main.py
@@ -1,16 +1,21 @@
-
+from Battleships import Game, Board
+from AI import AI
 
 def main():
     # Setup game envs for computer and player
-    # comp_env = Game()
-    # player_env = Game()
+    comp_board = Board.Board(10)
+    player_board = Board.Board(10)
+
+    comp_env = Game.Game(comp_board, player_board)
+    player_env = Game.Game(player_board, comp_board)
     
     # Setup a "computer player" to run through MCTS before returning a move
-    # computer = AI() - class that wraps MCTS functionality.
+    computer = AI(comp_env, 10) # class that wraps MCTS functionality.
 
     # Start game loop, running until either environment is over.
-    # comp_done, player_done = False, False
-    # while comp_done and player_done:
+    comp_done, player_done = False, False
+    while comp_done and player_done:
+        pass
         """
         make computer move, returning new_state, if game is done
         Do the same for the player, taking input from the user.
