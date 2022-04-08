129d56b3cfcc862a0d9bd363f25ad0f232f56518
Michael Bailey <rgfb35@durham.ac.uk>
2022-04-08 09:02:21 +0100

    Implement coordinate guess for shio placement

---
+++ MCT/Simulation.py
@@ -0,0 +1,34 @@
+import numpy as np
+import sys
+sys.path.append("./Battleships")
+import Board
+
+def randomly_place_ship(state, ship_length):
+    # Generate random, contiguous coordinates for the given ship length.
+    valid = False
+    while not valid:
+        guessed_coords = []
+
+        orientation = np.random.randint(0, 2) # 0 =x, 1= y(parts of the coordinates that don't change)
+        c1 = c2 = 0
+        while abs(c1 - c2) != ship_length - 1: # check to see if coordinates are sufficiently apart to "fill in" the rest: e.g., ship length = 3, then 2 and 4 with 3 in between for the cooridantes.
+            axis_val, c1, c2 = np.random.randint(0, state.width), np.random.randint(0, state.width), np.random.randint(0, state.width) # state should be passed in Board() instance.
+        
+        if c2 < c1: # switch coordinate 2 to be the upper bound
+            c1, c2 = c2, c1
+
+        if orientation == 0:
+            for num in range(c1, c2+1):
+                guessed_coords.append((axis_val, num))
+        else:
+            for num in range(c1, c2+1):
+                guessed_coords.append((num, axis_val))
+
+        # Next check conditions of validiity: if a "miss" is included and if there is at least one undiscovered square.
+
+
+    
+
+
+"""b = Board.Board(5)
+print(randomly_place_ship(b, 3))"""
